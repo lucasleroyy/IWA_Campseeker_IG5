dc09a6e72ee23c43d1666dd0b467218ccc874320
lucasleroyy <Lucas.leroy02@etu.umontpellier.fr>
2024-09-27 15:10:46 +0200

    ajout test

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello, Welcome to Campseeker!!!!!!!</Text>
+      <Text style={styles.title}>Hello, Welcome to Campseeker!!!!!</Text>
       <Text style={styles.subtitle}>This is a test page to check if everything is working correctly.</Text>
       <StatusBar style="auto" />
     </View>
