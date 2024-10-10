359fa4e0944a234c91d7a58460f6c5049d704b72
lucasleroyy <Lucas.leroy02@etu.umontpellier.fr>
2024-10-10 10:00:35 +0200

    contour orange

--- src/components/Boite_blanche.js
+++ src/components/Boite_blanche.js
@@ -17,6 +17,8 @@ const styles = StyleSheet.create({
     width: '90%', // Largeur fixe à 90% de l'écran pour éviter les chevauchements
     alignSelf: 'center', // Centre horizontalement la boîte
     marginVertical: 10, // Espace en haut et en bas
+    borderWidth: 2, // Taille du contour
+    borderColor: '#FF6D00', // Couleur orange pour le contour
     flex: 0, 
   },
 });
