02e9503c181faf1ce7c906cb9b3142ba044b4183
lucasleroyy <Lucas.leroy02@etu.umontpellier.fr>
2024-10-13 18:00:46 +0200

    ajout page profil

--- App.js
+++ App.js
@@ -1,35 +1,23 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import PageInscription from './src/Pages/PageInscription';
-import PageConnexion from './src/Pages/PageConnexion';
-import PageConditions from './src/Pages/PageConditions'; // Import de la page
+
+import PageProfil from './src/Pages/PageProfil'; // Import de la page
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="PageInscription">
+      <Stack.Navigator initialRouteName="PageProfil">
         <Stack.Screen 
-          name="PageInscription" 
-          component={PageInscription} 
+          name="PageProfil" 
+          component={PageProfil} 
           options={{ headerShown: false }} 
-        />
-        <Stack.Screen 
-          name="PageConnexion" 
-          component={PageConnexion} 
-          options={{ headerShown: false }} 
-        />
-        <Stack.Screen 
-          name="PageConditions" 
-          component={PageConditions} 
-          options={{ title: 'Conditions d\'utilisation' }} 
         />
       </Stack.Navigator>
     </NavigationContainer>
   );
-  
 };
 
 export default App;
