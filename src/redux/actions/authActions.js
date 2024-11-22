0a6dd3b85c4c04e900ea846484aaf4a4f4671464
Auriane Poirier <poirierauriane@gmail.com>
2024-11-22 15:09:37 +0100

    userId sauvegarder dans redux lors de la connexion

--- src/redux/actions/authActions.js
+++ src/redux/actions/authActions.js
@@ -1,30 +1,32 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
+export const authenticateUser = createAsyncThunk(
+  'user/authenticate',
+  async ({ email, password }, thunkAPI) => {
+    const apiUrl = thunkAPI.getState().config.apiUrl;
+    try {
+      const response = await fetch(`${apiUrl}/auth/login`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password }),
+      });
 
-export const authenticateUser = createAsyncThunk('user/authenticate', async ({ email, password }, thunkAPI) => {
-  const apiUrl = thunkAPI.getState().config.apiUrl;
-  try {
-    const response = await fetch(`${apiUrl}/auth/login`, { 
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ email, password })
-    });    
-    const data = await response.json();
-    console.log('Response:', data);
-    if (response.ok) {
-      console.log('Login successful', data);
-      return { userInfo: data, isAdmin: data.isAdmin };
-    } else {
-      console.log('Login failed', data);
-      return thunkAPI.rejectWithValue(data);
+      const data = await response.json();
+      if (response.ok) {
+        console.log('Réponse API de connexion :', data);
+        // Inclut le userId et le token dans la payload
+        return { userInfo: { userId: data.userId, token: data.token } };
+      } else {
+        console.error('Erreur API de connexion :', data);
+        return thunkAPI.rejectWithValue(data);
+      }
+    } catch (error) {
+      console.error('Erreur réseau :', error.message);
+      return thunkAPI.rejectWithValue(error.message);
     }
-  } catch (error) {
-    console.log('Network or other error', error);
-    return thunkAPI.rejectWithValue(error.message);
   }
-});
+);
+
 
 export const logout = () => ({
   type: 'LOGOUT'
